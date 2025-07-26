97087a37ab0a6e9e10641e90d6f8c93d1c3e7002
Hazem Mohamed <hazemm7622@gmail.com>
2025-07-26 10:57:17 +0300

    initialize pagination

---
+++ Movies.Contracts/Requests/PageRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.Contracts.Responses;
+
+public class PageSize
+{
+    public required int Page { get; init; }
+    
+    public required int pageSize { get; init; }
+    
+    
+}
---
+++ Movies.Contracts/Responses/PageResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Movies.Contracts.Responses;
+
+public class PageResponse<TResponse>
+{
+    public required IEnumerable<TResponse> Items { get; init; } = Enumerable.Empty<TResponse>();
+    public required int Page { get; init; } 
+       
+    public required int pageSize { get; init; }  
+    public required int total { get; init; }
+    
+    public bool HasNextPage => total > (Page * pageSize);
+}
